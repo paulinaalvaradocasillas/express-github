ce1ae235a5506f29698efcadecf8d80618f41e0d
Alvarado0811 <alvaradocasillaspaulina@gmail.com>
2026-03-26 19:26:45 -0700

    Finalizado: Express conectado a PostgreSQL y ruta principal funcionando

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express'); 
+const app = express();             
+const pool = require('./db');       
+
+
+pool.connect()
+  .then(() => {
+    console.log('✅ Conexión exitosa a PostgreSQL');
+  })
+  .catch((err) => {
+    console.error('❌ Error de conexión:', err);
+  });
+
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando');
 });
 
+
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+  console.log('🚀 Servidor corriendo en http://localhost:3000');
 });
