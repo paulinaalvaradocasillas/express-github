5e980e3e0ec6cef2d0583c6b5ecc3e21406bc7e7
Alvarado0811 <alvaradocasillaspaulina@gmail.com>
2026-04-14 20:42:27 -0700

    Actividad PostrgreSQL y express finalizada

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express'); 
-const app = express();             
-const pool = require('./db');       
+const express = require('express');
+const pool = require('./db');
+const app = express();
 
-
-pool.connect()
-  .then(() => {
-    console.log('✅ Conexión exitosa a PostgreSQL');
-  })
-  .catch((err) => {
-    console.error('❌ Error de conexión:', err);
-  });
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
+});
 
 app.listen(3000, () => {
-  console.log('🚀 Servidor corriendo en http://localhost:3000');
+  console.log('Servidor corriendo en http://localhost:3000');
 });
